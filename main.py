1053788b58131a9d11c47525e07e085526f87847
mmonary <50338273+mmonary@users.noreply.github.com>
2023-09-16 17:08:35 -0700

    Add files via upload

--- main.py
+++ main.py
@@ -1,71 +1,96 @@
-# Import Dependencies
+# Import dependencies
 import os, csv
 from pathlib import Path 
 
-# Declare file location through pathlib
-input_file = Path("Mod 3 Challenge", "PyBank", "budget_data.csv")
+# Assign file location with the pathlib library
+csv_file_path = Path("Mod 3 Challenge", "PyPoll", "election_data.csv")
 
-# Create empty lists to iterate through specific rows for the following variables
-total_months = []
-total_profit = []
-monthly_profit_change = []
- 
+# Declare Variables 
+total_votes = 0 
+khan_votes = 0
+correy_votes = 0
+li_votes = 0
+otooley_votes = 0
+
 # Open csv in default read mode with context manager
-with open(input_file,newline="", encoding="utf-8") as budget:
+with open(csv_file_path,newline="", encoding="utf-8") as elections:
 
-     # Store the contents of budget_data.csv in the variable csvreader
-    csvreader = csv.reader(budget,delimiter=",") 
+    # Store data under the csvreader variable
+    csvreader = csv.reader(elections,delimiter=",") 
 
-    # Skip the header labels to iterate with the values
-    header = next(csvreader)  
+    # Skip the header so we iterate through the actual values
+    header = next(csvreader)     
 
-    # Iterate through the rows in the stored file contents
+    # Iterate through each row in the csv
     for row in csvreader: 
 
-        # Append the total months and total profit to their corresponding lists
-        total_months.append(row[0])
-        total_profit.append(int(row[1]))
+        # Count the unique Voter ID's and store in variable  called total_votes
+        total_votes +=1
 
-    # Iterate through the profits in order to get the monthly change in profits
-    for i in range(len(total_profit)-1):
-        
-        # Take the difference between two months and append to monthly profit change
-        monthly_profit_change.append(total_profit[i+1]-total_profit[i])
-        
-# Obtain the max and min of the the montly profit change list
-max_increase_value = max(monthly_profit_change)
-max_decrease_value = min(monthly_profit_change)
+        # We have four candidates if the name is found, count the times it appears and store in a list
+        # We can use this values in our percent vote calculation in the print statements
+        if row[2] == "Khan": 
+            khan_votes +=1
+        elif row[2] == "Correy":
+            correy_votes +=1
+        elif row[2] == "Li": 
+            li_votes +=1
+        elif row[2] == "O'Tooley":
+            otooley_votes +=1
 
-# Correlate max and min to the proper month using month list and index from max and min
-max_increase_month = monthly_profit_change.index(max(monthly_profit_change)) + 1
-max_decrease_month = monthly_profit_change.index(min(monthly_profit_change)) + 1 
+ # To find the winner we want to make a dictionary out of the two lists we previously created 
+candidates = ["Khan", "Correy", "Li","O'Tooley"]
+votes = [khan_votes, correy_votes,li_votes,otooley_votes]
 
-# Print Statements
+# We zip them together the list of candidate(key) and the total votes(value)
+# Return the winner using a max function of the dictionary 
+dict_candidates_and_votes = dict(zip(candidates,votes))
+key = max(dict_candidates_and_votes, key=dict_candidates_and_votes.get)
 
-print("Financial Analysis")
-print("----------------------------")
-print(f"Total Months: {len(total_months)}")
-print(f"Total: ${sum(total_profit)}")
-print(f"Average Change: {round(sum(monthly_profit_change)/len(monthly_profit_change),2)}")
-print(f"Greatest Increase in Profits: {total_months[max_increase_month]} (${(str(max_increase_value))})")
-print(f"Greatest Decrease in Profits: {total_months[max_decrease_month]} (${(str(max_decrease_value))})")
+# Print a the summary of the analysis
+khan_percent = (khan_votes/total_votes) *100
+correy_percent = (correy_votes/total_votes) * 100
+li_percent = (li_votes/total_votes)* 100
+otooley_percent = (otooley_votes/total_votes) * 100
+
+# Print the summary table
+print(f"Election Results")
+print(f"----------------------------")
+print(f"Total Votes: {total_votes}")
+print(f"----------------------------")
+print(f"Khan: {khan_percent:.3f}% ({khan_votes})")
+print(f"Correy: {correy_percent:.3f}% ({correy_votes})")
+print(f"Li: {li_percent:.3f}% ({li_votes})")
+print(f"O'Tooley: {otooley_percent:.3f}% ({otooley_votes})")
+print(f"----------------------------")
+print(f"Winner: {key}")
+print(f"----------------------------")
 
 # Output files
-output_file = Path("python-challenge", "PyBank", "Fin_Analysis_Sum.txt")
+# Assign output file location and with the pathlib library
+output_file = Path("python-challenge", "PyPoll", "Election_Results_Summary.txt")
 
 with open(output_file,"w") as file:
-    
-# Write methods to print to Financial_Analysis_Summary 
-    file.write("Financial Analysis")
+
+# Write methods to print to Elections_Results_Summary 
+    file.write(f"Election Results")
     file.write("\n")
-    file.write("----------------------------")
+    file.write(f"----------------------------")
     file.write("\n")
-    file.write(f"Total Months: {len(total_months)}")
+    file.write(f"Total Votes: {total_votes}")
     file.write("\n")
-    file.write(f"Total: ${sum(total_profit)}")
+    file.write(f"----------------------------")
     file.write("\n")
-    file.write(f"Average Change: {round(sum(monthly_profit_change)/len(monthly_profit_change),2)}")
+    file.write(f"Khan: {khan_percent:.3f}% ({khan_votes})")
     file.write("\n")
-    file.write(f"Greatest Increase in Profits: {total_months[max_increase_month]} (${(str(max_increase_value))})")
+    file.write(f"Correy: {correy_percent:.3f}% ({correy_votes})")
     file.write("\n")
-    file.write(f"Greatest Decrease in Profits: {total_months[max_decrease_month]} (${(str(max_decrease_value))})")
+    file.write(f"Li: {li_percent:.3f}% ({li_votes})")
+    file.write("\n")
+    file.write(f"O'Tooley: {otooley_percent:.3f}% ({otooley_votes})")
+    file.write("\n")
+    file.write(f"----------------------------")
+    file.write("\n")
+    file.write(f"Winner: {key}")
+    file.write("\n")
+    file.write(f"----------------------------")
